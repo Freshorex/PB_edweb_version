48478f8f43c88511c451c200a839db32f9294616
Freshorex <dafreshestartist2em@gmail.com>
2026-02-09 17:39:15 +0100

    Fixed UI for phone version with js

--- JS/javacode.js
+++ JS/javacode.js
@@ -1,4 +1,19 @@
-document.getElementById("menu").onclick = function() {
-  document.getElementById("intmenu").style.display = "block";
-};
+ // Track the toggle state
+  let isActive = false;
 
+  // Get elements
+  const box = document.getElementById('intmenu');
+  const btn = document.getElementById('menu');
+
+  // Click event
+  btn.addEventListener('click', function () {
+    if (!isActive) {
+      // First click action
+      document.getElementById('intmenu').style.display = "block"
+    } else {
+      // Second click reverses the action
+      document.getElementById('intmenu').style.display = "none"
+    }
+    // Flip the state
+    isActive = !isActive;
+  });
