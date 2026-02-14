86106fd5ed3aced8da607922f3685c9fb735b133
Freshorex <dafreshestartist2em@gmail.com>
2026-02-14 01:24:48 +0100

    Continued templates (comics page, unfinished), need to add js stuff for the 3 pages that've been continued, cast page still needs non-trad accordeon and so does comics page

--- JS/javacode.js
+++ JS/javacode.js
@@ -1,8 +1,7 @@
-document.getElementById("menu").onclick = function() {
-document.getElementById("intmenu").style.display = "block";
-};
+ // Track the toggle state
+  let isActive = false;
 
-  // Get elements
+// Get elements
   const box = document.getElementById('intmenu');
   const btn = document.getElementById('menu');
 
@@ -18,3 +17,28 @@ document.getElementById("intmenu").style.display = "block";
     // Flip the state
     isActive = !isActive;
   });
+
+ // Track the toggle state
+  let isActive2 = false;
+
+// Get elements
+  const epbox = document.getElementById('ep-box');
+  const epbtn = document.getElementById('ep-btn');
+
+  // Click event
+  epbtn.addEventListener('click', function () {
+    if (!isActive2) {
+      // First click action
+      document.getElementById('ep-box').style.display = "block"
+    } else {
+      // Second click reverses the action
+      document.getElementById('ep-box').style.display = "none"
+    }
+    // Flip the state
+    isActive2 = !isActive2;
+  });
+
+
+
+
+
