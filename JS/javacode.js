0d672b871d7913e97df93b7ae87d4146517b6c37
Freshorex <dafreshestartist2em@gmail.com>
2026-05-12 19:13:42 +0200

    Blog page modals done, no content added however

--- JS/javacode.js
+++ JS/javacode.js
@@ -1,48 +1,63 @@
- // Track the toggle state
-  let isActive = false;
+// Track the toggle state
+let isActive = false;
 
 // Get elements
-  const box = document.getElementById('intmenu');
-  const btn = document.getElementById('menu');
+const box = document.getElementById('intmenu');
+const btn = document.getElementById('menu');
 
-  // Click event
-  btn.addEventListener('click', function () {
-    if (!isActive) {
-      // First click action
-      document.getElementById('intmenu').style.display = "block"
-    } else {
-      // Second click reverses the action
-      document.getElementById('intmenu').style.display = "none"
-    }
-    // Flip the state
-    isActive = !isActive;
-  });
+// Click event
+btn.addEventListener('click', function () {
+  if (!isActive) {
+    // First click action
+    document.getElementById('intmenu').style.display = "block"
+  } else {
+    // Second click reverses the action
+    document.getElementById('intmenu').style.display = "none"
+  }
+  // Flip the state
+  isActive = !isActive;
+});
 
- // Track the toggle state
-  let isActive2 = false;
+// Track the toggle state
+let isActive2 = false;
 
 // Get elements
-  const epbox = document.getElementById('ep-box');
-  const epbtn = document.getElementById('ep-btn');
+const epbox = document.getElementById('ep-box');
+const epbtn = document.getElementById('ep-btn');
 
-  // Click event
-  epbtn.addEventListener('click', function () {
-    if (!isActive2) {
-      // First click action
-      document.getElementById('ep-box').style.display = "block"
-    } else {
-      // Second click reverses the action
-      document.getElementById('ep-box').style.display = "none"
-    }
-    // Flip the state
-    isActive2 = !isActive2;
-  });
+// Click event
+epbtn.addEventListener('click', function () {
+  if (!isActive2) {
+    // First click action
+    document.getElementById('ep-box').style.display = "block"
+  } else {
+    // Second click reverses the action
+    document.getElementById('ep-box').style.display = "none"
+  }
+  // Flip the state
+  isActive2 = !isActive2;
+});
 
- function hide() {
-        document.getElementById('homevideo').style.display="none";  // for hide button
-        }
+function hide() {
+  document.getElementById('homevideo').style.display = "none";  // for hide button
+}
 
- function show() {
-        document.getElementById('homevideo').style.display="block";  // for hide button
-        }
+function show() {
+  document.getElementById('homevideo').style.display = "block";  // for hide button
+}
 
+function mhide2() {
+  document.getElementById('newzmid2').style.display = "none";
+}
+
+function mshow2() {
+  document.getElementById('newzmid2').style.display = "block";
+}
+
+function mhide1() {
+  document.getElementById('newzmid1').style.display = "none";
+}
+
+function mshow1() {
+  document.getElementById('newzmid1').style.display = "block";
+}
